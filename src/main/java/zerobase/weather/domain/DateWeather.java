3717d97c2cfff95cdffb39970bba0bd7ba3ec0cc
Goni <dbtnrhs410@naver.com>
2024-01-08 02:32:00 +0900

    weather

---
+++ src/main/java/zerobase/weather/domain/DateWeather.java
@@ -0,0 +1,23 @@
+package zerobase.weather.domain;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity(name = "date_weather")
+public class DateWeather {
+    @Id
+    private LocalDate date;
+    private String weather;
+    private String icon;
+    private double temperature;
+}
